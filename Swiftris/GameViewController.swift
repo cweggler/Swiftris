e6b84d9b7923fc34c7ec3ef38b3e1432cfb5eb14
Cara Weggler <cweggler@gmail.com>
2018-04-12 20:47:34 -0500

    created SpriteKit's skView so that the background could be displayed

--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
+    var scene: GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
+        // Configure the view.
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = false
+        
+        // Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        
+        // Present the scene.
+        skView.presentScene(scene)
+        
     }
 
     override var prefersStatusBarHidden: Bool {
