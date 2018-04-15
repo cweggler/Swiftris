115394ab98fcf55b16e199e7d1ac896a44413f67
Cara Weggler <cweggler@gmail.com>
2018-04-15 13:01:45 -0500

    Created Array2D file to create the game's board layout with an array

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,32 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by Cara on 4/14/18.
+//  Copyright © 2018 Bloc. All rights reserved.
+//
+
+class Array2D<T>{
+    let columns : Int
+    let rows: Int
+    
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int){
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(repeating: nil, count: rows * columns)
+        
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get{
+            return array[(row * columns) + column]
+        }
+        
+        set(newValue){
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
