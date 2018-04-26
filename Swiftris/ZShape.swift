55ea463f405ab9230ca8fa94c4214ea62a7c73b2
Cara Weggler <cweggler@gmail.com>
2018-04-25 19:00:38 -0500

    created the ZShape subclass so I have that block object to work with

--- Swiftris/ZShape.swift
+++ Swiftris/ZShape.swift
@@ -6,4 +6,55 @@
 //  Copyright © 2018 Bloc. All rights reserved.
 //
 
-import Foundation
+class ZShape:Shape {
+    
+    /*
+        Orientation 0
+     
+          * | 0 |
+        | 2 | 1 |
+        | 3 |
+     
+        Orientation 90
+     
+        | 3 | *2 |
+             | 1 | 0 |
+     
+        Orientation 180
+     
+          * | 0 |
+        | 2 | 1 |
+        | 3 |
+     
+        Orientation 270
+     
+        | 3 | *2 |
+             | 1 | 0 |
+    */
+    
+    /* Have to override these properties as set up in
+     the Shape class
+     */
+    
+    override var blockRowColumnPositions: [Orientation : Array<(columnDiff: Int, rowDiff: Int)>] {
+        
+        return [
+            
+            Orientation.Zero: [(1,0), (1,1), (0,1), (0,2)],
+            Orientation.Ninety: [(1,1), (0,1), (0,0), (-1,0)],
+            Orientation.OneEighty: [(1,0), (1,1), (0,1), (0,2)],
+            Orientation.TwoSeventy: [(1,1), (0,1), (0,0), (-1,0)]
+        ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation : Array<Block>] {
+        
+        return [
+            
+            Orientation.Zero: [blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.Ninety: [blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.OneEighty: [blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy:[blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[FourthBlockIdx]]
+        ]
+    }
+}
