6e151dbc9200f384dd455c2f4fd8a0bce2392b23
Cara Weggler <cweggler@gmail.com>
2018-04-30 12:09:42 -0500

    created a GameLogic class to put logic of what is a falling block and what block comes next

---
+++ Swiftris/GameLogic.swift
@@ -0,0 +1,44 @@
+//
+//  GameLogic.swift
+//  Swiftris
+//
+//  Created by Cara on 4/30/18.
+//  Copyright © 2018 Bloc. All rights reserved.
+//
+
+let NumColumns = 10
+let NumRows = 20
+
+let StartingColumn = 4
+let StartingRow = 0
+
+let PreviewColumn = 12
+let PreviewRow = 1
+
+class GameLogic {
+    var blockArray: Array2D<Block>
+    var nextShape: Shape?
+    var fallingShape: Shape?
+    
+    init() {
+        fallingShape = nil
+        nextShape = nil
+        blockArray = Array2D<Block>(columns: NumColumns, rows: NumRows)
+    }
+    
+    func beginGame() {
+        if (nextShape == nil){
+            nextShape = Shape.random(startingColumn: PreviewColumn, startingRow: PreviewRow)
+        }
+    }
+    
+    func newShape() -> (fallingShape: Shape?, nextShape: Shape?) {
+        fallingShape = nextShape
+        nextShape = Shape.random(startingColumn: PreviewColumn, startingRow: PreviewRow)
+        fallingShape?.moveTo(column: StartingColumn, row: StartingRow)
+        
+        return(fallingShape, nextShape)
+    }
+}
+
+
