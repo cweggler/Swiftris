45e60272bd3c6197924c102f65a9803514b5a62d
Cara Weggler <cweggler@gmail.com>
2018-04-30 16:22:49 -0500

    created functions for helping to the shapes to fall on the screen

--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -11,6 +11,7 @@ import SpriteKit
 
 class GameViewController: UIViewController {
     var scene: GameScene!
+    var swiftris: GameLogic!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,12 +24,31 @@ class GameViewController: UIViewController {
         scene = GameScene(size: skView.bounds.size)
         scene.scaleMode = .aspectFill
         
+        scene.tick = didTick
+        
+        swiftris = GameLogic()
+        swiftris.beginGame()
+        
+        
         // Present the scene.
         skView.presentScene(scene)
         
+        scene.addPreviewShapeToScene(shape: swiftris.nextShape!){
+            self.swiftris.nextShape?.moveTo(column: StartingColumn, row: StartingRow)
+            self.scene.movePreviewShape(shape: self.swiftris.nextShape!){
+                let nextShapes = self.swiftris.newShape()
+                self.scene.startTicking()
+                self.scene.addPreviewShapeToScene(shape: nextShapes.nextShape!) {}
+            }
+        }
     }
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    func didTick() {
+        swiftris.fallingShape?.lowerShapeByOneRow()
+        scene.redrawShape(shape: swiftris.fallingShape!, completion: {})
     }
 }
