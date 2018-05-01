b4880f09b629dcd1d8d60a70a7d4266720907c8e
Cara Weggler <cweggler@gmail.com>
2018-04-30 19:10:56 -0500

    Added functions that will move the blocks around and rotate clockwise and counterclockwise

--- Swiftris/Shape.swift
+++ Swiftris/Shape.swift
@@ -156,8 +156,32 @@ class Shape: Hashable, CustomStringConvertible {
         }
     }
     
+    final func rotateClockwise() {
+        let newOrientation = Orientation.rotate(orientation: orientation, clockwise: true)
+        rotateBlocks(orientation: newOrientation)
+        orientation = newOrientation
+    }
+    
+    final func rotateCounterClockwise() {
+        let newOrientation = Orientation.rotate(orientation: orientation, clockwise: false)
+        rotateBlocks(orientation: newOrientation)
+        orientation = newOrientation
+    }
+    
     final func lowerShapeByOneRow() {
         shiftBy(columns:0, rows:1)
+    }
+    
+    final func raiseShapeByOneRow() {
+        shiftBy(columns: 0, rows: -1)
+    }
+    
+    final func shiftRightByOneColumn() {
+        shiftBy(columns: 1, rows: 0)
+    }
+    
+    final func shiftLeftByOneColumn() {
+        shiftBy(columns: -1, rows: 0)
     }
     
     final func shiftBy(columns: Int, rows: Int){
